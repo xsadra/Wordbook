880ef014980bdd5f984a0afebca3e7ccd41c019a
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-25 18:26:52 +0200

    Add singleton WordDatabase abstract class,

---
+++ app/src/main/java/at/sadra/apps/wordbook/room/dao/WordDatabase.java
@@ -0,0 +1,25 @@
+package at.sadra.apps.wordbook.room.dao;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import at.sadra.apps.wordbook.app.App;
+
+public abstract class WordDatabase extends RoomDatabase {
+    private static WordDatabase instance;
+
+    public static synchronized WordDatabase getInstance(Context context) {
+
+        if (instance == null) {
+            Room.databaseBuilder(context, WordDatabase.class, App.Values.DATABASE_NAME)
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+
+        return instance;
+    }
+
+    public abstract WordDao wordDao();
+}
