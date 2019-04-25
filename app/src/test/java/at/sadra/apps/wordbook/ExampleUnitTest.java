66815e17a6ad5ccd80348b69f2a1fecb5dd5ea90
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-25 12:01:18 +0200

    test the Junit 5

--- app/src/test/java/at/sadra/apps/wordbook/ExampleUnitTest.java
+++ app/src/test/java/at/sadra/apps/wordbook/ExampleUnitTest.java
@@ -1,8 +1,11 @@
 package at.sadra.apps.wordbook;
 
-import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -14,4 +17,16 @@ public class ExampleUnitTest {
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
     }
+
+
+    @Test
+    public void testDivide() {
+        assertThrows(ArithmeticException.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                Integer.divideUnsigned(42, 0);
+            }
+        });
+    }
 }
+
